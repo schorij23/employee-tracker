494da38b7398747e621c61d32e81d2ea0232e4de
schorij23 <schorij23@gmail.com>
2023-11-01 19:48:53 -0400

    update schema,server

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,26 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 -- Uses the database
 USE employee_tracker;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+);
+
+
+
