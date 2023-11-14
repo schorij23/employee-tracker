f8044ca85c2e73b36b33350a90a641160988118c
schorij23 <schorij23@gmail.com>
2023-11-14 02:07:18 -0500

    more seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,17 +5,22 @@ INSERT INTO department (name) VALUES
 ('IT'),
 ('ACCOUNTING'),
 ('LEGAL'),
-('SALES');
+('SALES'),
+('ORGANIZATIONAL');
 INSERT INTO role (title, salary, department_id) VALUES 
-('Manager', 60000, 1),
-('Lawyer', 50000, 4),
-('Sales Lead', 50000, 3),
-('Salesperson', 30000, 3),
-('Lead Engineer', 70000, 2);
+('Manager', 80000, 1),
+('Lawyer', 70000, 4),
+('Sales Lead', 50000, 5),
+('Salesperson', 30000, 5),
+('Lead Engineer', 70000, 2),
+('Administrative Assistant', 40000, 6),
+('Accountant', 60000, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Smith', 1, NULL), 
 ('Dave', 'Johnson', 3, NULL),
 ('Micheal', 'Jordan', 2, 1),
 ('Steve', 'Smith', 5, NULL),
 ('Micheal', 'Jones', 3, 3),
-('Micheal', 'Smith', 3, 3);
+('Micheal', 'Smith', 3, 3),
+('Mike', 'Jones', 7, NULL),
+('Shara', 'Clark', 6, NULL);
